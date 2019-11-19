7760149f30b1997de92b8d754e98e91f1116ead6
Jeremy Tsang <jeremy@jeremytsang.com>
2019-11-19 01:16:10 -0800

    Make window use white background

--- src/interface.py
+++ src/interface.py
@@ -12,6 +12,15 @@ game_display = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Circle Game')
 clock = pygame.time.Clock()
 
+# RGB Colors
+# RGB colors
+colors = {
+    "black": (0, 0, 0),
+    "white": (255, 255, 255),
+    "red": (255, 0, 0),
+    "green": (0, 255, 0),
+    "blue": (0, 0, 255)}
+
 crashed = False  # When to quit
 
 while not crashed:
@@ -19,6 +28,7 @@ while not crashed:
         if event.type == pygame.QUIT:
             crashed = True
 
+    game_display.fill(colors['white'])
     pygame.display.update()
     clock.tick(30)
 
