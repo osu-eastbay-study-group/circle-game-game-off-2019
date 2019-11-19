0634b8cf2a079cc42afe06b26d7bce20d028031e
Jeremy Tsang <jeremy@jeremytsang.com>
2019-11-19 01:32:18 -0800

    Make red dot move via polar coordinates
    Allow user to move red dot by increasing/decreasing r and theta via
    the arrow keys

--- src/interface.py
+++ src/interface.py
@@ -1,4 +1,5 @@
 import pygame
+import coordinateconverter
 
 # Avoid hard coding resolution
 display_width = 800
@@ -20,12 +21,21 @@ colors = {
     "green": (0, 255, 0),
     "blue": (0, 0, 255)}
 
-# Image that user moves
-dot_img = pygame.image.load('../img/red_dot.png')
+dot_img = pygame.image.load('../img/red_dot.png')  # Image that user moves
+
+# Position related
+converter = coordinateconverter.CoordinateConverter(display_width,
+                                                    display_height)
+r = 0
+theta = 0
+r_change = 0
+theta_change = 0
+
 
 # Display the image
 def dot(x_pixel, y_pixel):
     game_display.blit(dot_img, (x_pixel, y_pixel))
+
 
 crashed = False  # When to quit
 
@@ -34,8 +44,27 @@ while not crashed:
         if event.type == pygame.QUIT:
             crashed = True
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                theta_change = -5  # move clockwise
+                print("←")
+            if event.key == pygame.K_RIGHT:
+                theta_change = 5  # move counterclockwise
+                print("→")
+            if event.key == pygame.K_DOWN:
+                r_change = -5  # move towards origin
+                print("↓")
+            if event.key == pygame.K_UP:
+                r_change = 5   # move away from origin
+                print("↑")
+
+    # Update the pixel coordinates of the image
+    r += r_change
+    theta += theta_change
+    x_pixel, y_pixel = converter.cartesian_to_pixel(converter.polar_to_cartesian((r, theta)))
+
     game_display.fill(colors['white'])
-    dot(display_width // 2, display_height // 2)  # show in middle of window
+    dot(x_pixel, y_pixel)  # show in middle of window
     pygame.display.update()
     clock.tick(30)
 
