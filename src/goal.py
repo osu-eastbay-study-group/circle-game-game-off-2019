6a4a7d0ead640b0b6f80730f3809b5d164db2ca2
Jeremy Tsang <jeremy@jeremytsang.com>
2019-11-29 03:08:49 -0800

    Add clas Goal docstrings

--- src/goal.py
+++ src/goal.py
@@ -2,7 +2,11 @@ from src.polarplaceable import PolarPlaceable
 
 
 class Goal(PolarPlaceable):
+    """
+    Class representing the objects the player must collect to earn points.
+    """
     def __init__(self, radius_list, radius_level, theta, color, points=1):
+        """Creates a object of type Goal with a point value."""
         super().__init__(radius_list, radius_level, theta, color)
         self._points = points
 
