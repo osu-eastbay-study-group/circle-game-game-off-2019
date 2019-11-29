823ed49e7a147180f4165145c492f101ab1f6710
Hae-Ji Park <positive235@gmail.com>
2019-11-29 01:13:59 -0800

    Player(pink dot) moves by keyboard while killer(red dot) and goal(green dot) moves automatically on the screen

--- src/circle_game.py
+++ src/circle_game.py
@@ -1,11 +1,12 @@
 import pygame
 from coordinateconverter import CoordinateConverter
 
-BLACK, WHITE, RED, GREEN, BLUE = (0, 0, 0), (255, 255, 255), (255, 0, 0), (0, 255, 0), (0, 0, 255)
+BLACK, WHITE, RED, \
+GREEN, BLUE, HOTPINK = (0, 0, 0), (255, 255, 255), (255, 0, 0), \
+                       (0, 255, 0), (0, 0, 255), (255, 105, 180)
 
 class CircleGame:
     def __init__(self):
-
         self.display_width = 800
         self.display_height = 600
         self.title = 'Circle Game'
@@ -16,14 +17,18 @@ class CircleGame:
         self.space_img = pygame.image.load('../img/space_img.jpg')
         self.screen.blit(self.space_img, self.space_img.get_rect())
 
-    def display_player(self, x_pixel, y_pixel):
-        self.x_pixel = x_pixel
-        self.y_pixel = y_pixel
-        self.player = pygame.draw.circle(self.screen, BLUE, (self.x_pixel, self.y_pixel), 10)
+    def display_dots(self, x, y, color):
+        self.x, self.y, self.color = x, y, color
+        pygame.draw.circle(self.screen, self.color, (self.x, self.y), 10)
 
-    def convert_dot(self, pygame, clock):
+    def dots_pos(self, rr, tt):
+        self.rr, self.tt = rr, tt
+        return self.converter.cartesian_to_pixel(self.converter.polar_to_cartesian((self.rr, self.tt)))
+
+    def dots_move(self, pygame, clock):
         self.pygame, self.clock = pygame, clock
         self.r, self.theta, self.r_change, self.theta_change = 0, 0, 0, 0
+        self.killer_theta, self.goal_theta = 0, 0
         self.converter = CoordinateConverter(self.display_width, self.display_height)
         self.exit = False
 
@@ -42,14 +47,15 @@ class CircleGame:
                         print("→")
 
                     if event.key == self.pygame.K_DOWN:
-                        self.r_change = -10  # move towards origin
+                        self.r_change = -50  # move towards origin
                         print("↓")
 
                     if event.key == self.pygame.K_UP:
-                        self.r_change = 10  # move away from origin
+                        self.r_change = 50  # move away from origin
                         print("↑")
 
                 if event.type == self.pygame.KEYUP:
+
                     if event.key in (self.pygame.K_LEFT, self.pygame.K_RIGHT):
                         self.theta_change = 0
                     if event.key in (self.pygame.K_DOWN, self.pygame.K_UP):
@@ -57,10 +63,17 @@ class CircleGame:
 
             self.r += self.r_change
             self.theta += self.theta_change
-            self.player_x, self.player_y = self.converter.cartesian_to_pixel(self.converter.polar_to_cartesian((self.r,
-                                                                                                             self.theta)))
+            self.killer_theta -= 10
+            self.goal_theta += 10
+            self.player_x, self.player_y = self.dots_pos(self.r, self.theta)
+            self.killer_x, self.killer_y = self.dots_pos(200, self.killer_theta)
+            self.goal_x, self.goal_y = self.dots_pos(150, self.goal_theta)
+
             self.screen_set()
-            self.display_player(self.player_x, self.player_y)
+
+            self.display_dots(self.player_x, self.player_y, HOTPINK)
+            self.display_dots(self.killer_x, self.killer_y, RED)
+            self.display_dots(self.goal_x, self.goal_y, GREEN)
 
             self.pygame.display.flip()
             self.clock.tick(30)
--- src/interface.py
+++ src/interface.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     pygame.init()
     circle_game = CircleGame()
     clock = pygame.time.Clock()
-    pygame, clock = circle_game.convert_dot(pygame, clock)
+    pygame, clock = circle_game.dots_move(pygame, clock)
     pygame.quit()
