308191f30eb7f437a371b9d7d1afccb51ad88671
Jeremy Tsang <jeremy@jeremytsang.com>
2019-11-29 03:40:12 -0800

    Make _points_collected member for class Player
    Update docstring for points too.

--- circlegame/characters/player.py
+++ circlegame/characters/player.py
@@ -2,7 +2,11 @@ from circlegame.characters.polarplaceable import PolarPlaceable
 
 
 class Player(PolarPlaceable):
-    """Class to represent the object that the player controls."""
+    """Class to represent the object that the player controls. Collects points during game."""
     def __init__(self, radius_list, radius_level, theta):
         """Create a player object with a HOTPINK color."""
         super().__init__(radius_list, radius_level, theta, "HOTPINK")
+        self._points_collected = 0
+
+    def get_points_collected(self):
+        return self._points_collected
