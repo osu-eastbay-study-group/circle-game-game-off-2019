cc83a6a2da249c5fe5decda6dcfc55ba87e784f1
Jeremy Tsang <jeremy@jeremytsang.com>
2019-11-29 03:27:27 -0800

    Create class Killer
    Create class of objects that can attack the player.

---
+++ circlegame/characters/killer.py
@@ -0,0 +1,8 @@
+from circlegame.characters.polarplaceable import PolarPlaceable
+
+
+class Killer(PolarPlaceable):
+    """Represents the characters that can attack the player."""
+    def __init__(self, radius_list, radius_level, theta, color):
+        super().__init__(radius_list, radius_level, theta, color)
+
