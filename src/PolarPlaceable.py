de37227ae3f1f6e54d05d94e1d9ebbdfba042f71
Jeremy Tsang <jeremy@jeremytsang.com>
2019-11-29 02:34:54 -0800

    Add docstrings for class PolarPlaceable

--- src/PolarPlaceable.py
+++ src/PolarPlaceable.py
@@ -1,11 +1,37 @@
 class PolarPlaceable():
+    """
+    Class to represent objects in the PyGame window with position given
+    with a radius index and a angle (theta).
+    """
     def __init__(self, radius_list, radius_level, theta, color):
+        """
+
+        Parameters
+        ----------
+        radius_list : list of float or int
+            List of permissible radius levels for the object to be located at.
+            Represents the concentric circles of the map.
+        radius_level : int
+            Index of the current radius the object is located at.
+        theta : float or int
+            Angle value in the polar coordinate (r, theta).
+        color : str
+            Color of the circle to draw.
+        """
         self._radius_list = radius_list
         self._radius_index = radius_level
         self._theta = theta
         self._color = color
 
     def get_draw_data(self):
+        """
+        Provides relevant data for PyGame to draw the object.
+
+        Returns
+        -------
+        Tuple of (int, float or int, str)
+            Index of the radius list, current theta value, and color string.
+        """
         return (self._radius_index, self._theta, self._color)
 
     def get_radius_index(self):
@@ -16,6 +42,8 @@ class PolarPlaceable():
 
     def get_color(self):
         return self._color
+
+
 
 
 if __name__ == "__main__":
