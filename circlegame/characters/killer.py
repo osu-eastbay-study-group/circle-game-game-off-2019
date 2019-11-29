468bee0b3ea3046eabff436b4ca199be2880050a
Jeremy Tsang <jeremy@jeremytsang.com>
2019-11-29 03:29:36 -0800

    Make killer have a red color
    Make the killer only need values for radius list, radius level, and
    theta for initialization.

--- circlegame/characters/killer.py
+++ circlegame/characters/killer.py
@@ -3,6 +3,6 @@ from circlegame.characters.polarplaceable import PolarPlaceable
 
 class Killer(PolarPlaceable):
     """Represents the characters that can attack the player."""
-    def __init__(self, radius_list, radius_level, theta, color):
-        super().__init__(radius_list, radius_level, theta, color)
+    def __init__(self, radius_list, radius_level, theta):
+        super().__init__(radius_list, radius_level, theta, "RED")
 
